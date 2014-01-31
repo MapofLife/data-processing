6fdee1d37b1a9e4d1a3ab7031654be0ca2fd03c0
Ijeamakaanyene <Ijeamaka.Anyene@gmail.com>
2014-01-31 14:43:07 -0500

    updated kukulugala_reptiles

--- staging/load_scripts/kukulugala_reptiles.sql
+++ staging/load_scripts/kukulugala_reptiles.sql
@@ -1,6 +1,6 @@
 ﻿--F5 - Execute selection
 --Replace All 
--- bellanwila-attidiya_herps with the species table name
+-- kukulugala_reptiles with the species table name
 -- karunarathna with provider e.g. 'stalmans'
 
 ---------------------------
@@ -13,16 +13,16 @@
 --example: golarainforest_herps
 
 --create the column that will link from the geom table (primary key) to species list (foreign key)
-alter table bellanwila_attidiya_herps
+alter table kukulugala_reptiles
 add column geom_id integer;
 
 -- create an index on the geom id <tablename>_<colname>_btree
-drop index if exists bellanwila_attidiya_herps_geom_id_btree;
-create index bellanwila_attidiya_herps_geom_id_btree ON bellanwila_attidiya_herps (geom_id);
+drop index if exists kukulugala_reptiles_geom_id_btree;
+create index kukulugala_reptiles_geom_id_btree ON kukulugala_reptiles (geom_id);
 
 --create an index on the scientific name  <tablename>_scientificname_btree
-drop index if exists bellanwila_attidiya_herps_scientificname_btree;
-create index bellanwila_attidiya_herps_scientificname_btree ON bellanwila_attidiya_herps (scientificname);
+drop index if exists kukulugala_reptiles_scientificname_btree;
+create index kukulugala_reptiles_scientificname_btree ON kukulugala_reptiles (scientificname);
 
 -------------------------------------------------------------------------
 -- fix data if required - better to do in excel and re-upload if possible
@@ -41,9 +41,8 @@ create index bellanwila_attidiya_herps_scientificname_btree ON bellanwila_attidi
 --use cartodb webui to create the geom table. <tablename>_geom  or use below "create table from query" option in cartodb.
 --this query will work if the is a single polygon for the checklist.
 
-select new_new_name as geom_name, the_geom
-from wdpa2010
-where new_new_name = 'Banhine'
+SELECT new_new_name as geom_name, the_geom 
+FROM wdpa2010 where wdpaid = 27915
 
 
 --3. used a supplied shapfile
@@ -51,7 +50,7 @@ where new_new_name = 'Banhine'
 -- create the geom table use "create table from query" button in cartodb
 -- merge multiple records into a single record
 select geom_name as geom_name, st_multi(st_union(the_geom)) as the_geom
-from bellanwila_attidiya_herps_shapefile
+from kukulugala_reptiles_shapefile
 group by geom_name;
 
 -------------------------------------------
@@ -62,8 +61,8 @@ group by geom_name;
 -- this query should come back empty
 -- not needed if there is only one geometry to link to
 SELECT  m.dist
-FROM    bellanwila_attidiya_herps m 
-LEFT JOIN bellanwila_attidiya_herps_geom g
+FROM    kukulugala_reptiles m 
+LEFT JOIN kukulugala_reptiles_geom g
 ON      g.geom_name = m.dist
 WHERE   g.geom_name IS NULL
 group by m.dist
@@ -71,17 +70,17 @@ group by m.dist
 -- could check the other way - are the rows in the geometry table that aren't in species table?
 
 --add in all of the id's from the geometry table.
-update bellanwila_attidiya_herps m
+update kukulugala_reptiles m
 set geom_id = g.cartodb_id
-from bellanwila_attidiya_herps_geom g
+from kukulugala_reptiles_geom g
 where m.dist = g.geom_name
 
 -- if there is only one geometry, set geom_id to the appropriate cartodb id
-update bellanwila_attidiya_herps m
+update kukulugala_reptiles m
 set geom_id = 1
 
 --set to not null.  extra check to make sure all rows matched.
-alter table bellanwila_attidiya_herps
+alter table kukulugala_reptiles
 alter column geom_id set not null;
 
 -- make species list and geom table public in cartodb
@@ -97,18 +96,18 @@ alter column geom_id set not null;
 -----------------------
 
 -- test in cartodb, get_tile should map te specis
-SELECT * FROM get_tile('karunarathna', 'localinv', 'Peliperdix coqui','bellanwila_attidiya_herps')
+SELECT * FROM get_tile('karunarathna', 'localinv', 'Calotes calotes','kukulugala_reptiles')
 
 -- sql to if get_tile does not work.
-SELECT * from data_registry WHERE provider = 'karunarathna' and type = 'localinv' or table_name = 'bellanwila_attidiya_herps'
+SELECT * from data_registry WHERE provider = 'karunarathna' and type = 'localinv' or table_name = 'kukulugala_reptiles'
 
 SELECT d.*,g.*
-  FROM bellanwila_attidiya_herps d
-  JOIN bellanwila_attidiya_herps_geom g ON 
+  FROM kukulugala_reptiles d
+  JOIN kukulugala_reptiles_geom g ON 
   d.geom_id = g.cartodb_id
   where d.scientificname = 'Azolla nilotica';
 
-select * from bellanwila_attidiya_herps where scientificname = 'Azolla nilotica';	
+select * from kukulugala_reptiles where scientificname = 'Azolla nilotica';	
 	  
 			  
 -------------------------------------------
@@ -116,35 +115,35 @@ select * from bellanwila_attidiya_herps where scientificname = 'Azolla nilotica'
 -------------------------------------------
 
 insert into layer_metadata_staging
-	select * from get_mol_layers('bellanwila_attidiya_herps');
+	select * from get_mol_layers('kukulugala_reptiles');
 	
 ---------------------------------
 -- populate the ac_staging table
 ---------------------------------
 
 --sanity check: both queries should have the same number of rows
-select distinct scientificname from bellanwila_attidiya_herps
+select distinct scientificname from kukulugala_reptiles
 
 select distinct m.scientificname as n,
 	t.common_names_eng as v
-from bellanwila_attidiya_herps m left join taxonomy t 
+from kukulugala_reptiles m left join taxonomy t 
 on m.scientificname = t.scientificname
 
 -- insert the rows
 insert into ac_staging
 	select distinct m.scientificname as n,
 		t.common_names_eng as v
-	from bellanwila_attidiya_herps m left join taxonomy t 
+	from kukulugala_reptiles m left join taxonomy t 
 	on m.scientificname = t.scientificname
 
 ---------------------------------
 -- dataset stats
 ---------------------------------
 
---num species: 306
+--num species: 58
 --num geometries: 1
 
 --species to test:
---Peliperdix coqui
+--Calotes calotes
 
 
