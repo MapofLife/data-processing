edd021d45b4c636e8b4a2e02dad4edd21a3b818a
Ijeamakaanyene <Ijeamaka.Anyene@gmail.com>
2014-02-26 23:11:19 -0500

    updated and finished india_reptiles sql and csv

---
+++ staging/load_scripts/india_reptiles.sql
@@ -0,0 +1,150 @@
+﻿--F5 - Execute selection
+--Replace All 
+-- india_reptiles with the species table name
+-- venugopal with provider e.g. 'stalmans'
+
+---------------------------
+-- create the species table
+---------------------------
+
+--upload the xlsx into cartodb
+
+--rename if necessary w/in cartodb ui.  format should be <place>_<taxa>
+--example: golarainforest_herps
+
+--create the column that will link from the geom table (primary key) to species list (foreign key)
+alter table india_reptiles
+add column geom_id integer;
+
+-- create an index on the geom id <tablename>_<colname>_btree
+drop index if exists india_reptiles_geom_id_btree;
+create index india_reptiles_geom_id_btree ON india_reptiles (geom_id);
+
+--create an index on the scientific name  <tablename>_scientificname_btree
+drop index if exists india_reptiles_scientificname_btree;
+create index india_reptiles_scientificname_btree ON india_reptiles (scientificname);
+
+-------------------------------------------------------------------------
+-- fix data if required - better to do in excel and re-upload if possible
+-------------------------------------------------------------------------
+
+
+------------------------
+-- create the geom table
+------------------------
+--1. usig gadm
+--2. using wdpa
+--3. using a shapefile
+
+--2. using wdpa
+
+--use cartodb webui to create the geom table. <tablename>_geom  or use below "create table from query" option in cartodb.
+--this query will work if the is a single polygon for the checklist.
+
+select iso as geom_name, the_geom
+from gadm2_country
+where iso = 'IND'
+
+
+--3. used a supplied shapfile
+
+-- create the geom table use "create table from query" button in cartodb
+-- merge multiple records into a single record
+select geom_name as geom_name, st_multi(st_union(the_geom)) as the_geom
+from india_reptiles_shapefile
+group by geom_name;
+
+-------------------------------------------
+-- link the species table to the geom table
+-------------------------------------------
+
+-- first check to see if any distribution rows don't match
+-- this query should come back empty
+-- not needed if there is only one geometry to link to
+SELECT  m.dist
+FROM    india_reptiles m 
+LEFT JOIN india_reptiles_geom g
+ON      g.geom_name = m.dist
+WHERE   g.geom_name IS NULL
+group by m.dist
+
+-- could check the other way - are the rows in the geometry table that aren't in species table?
+
+--add in all of the id's from the geometry table.
+update india_reptiles m
+set geom_id = g.cartodb_id
+from india_reptiles_geom g
+where m.dist = g.geom_name
+
+-- if there is only one geometry, set geom_id to the appropriate cartodb id
+update india_reptiles m
+set geom_id = 1
+
+--set to not null.  extra check to make sure all rows matched.
+alter table india_reptiles
+alter column geom_id set not null;
+
+-- make species list and geom table public in cartodb
+
+-----------------------
+-- populate all the surfacing tables
+-----------------------
+
+-- use other sql template
+
+-----------------------
+-- Test out get_tile, should show up in cartodb visualization
+-----------------------
+
+-- test in cartodb, get_tile should map te specis
+SELECT * FROM get_tile('venugopal', 'localinv', 'Bronchocela jubata','india_reptiles')
+
+-- sql to if get_tile does not work.
+SELECT * from data_registry WHERE provider = 'venugopal' and type = 'localinv' or table_name = 'india_reptiles'
+
+SELECT d.*,g.*
+  FROM india_reptiles d
+  JOIN india_reptiles_geom g ON 
+  d.geom_id = g.cartodb_id
+  where d.scientificname = 'Azolla nilotica';
+
+select * from india_reptiles where scientificname = 'Azolla nilotica';	
+	  
+			  
+-------------------------------------------
+-- populate the layer_metadata_staging table
+-------------------------------------------
+
+insert into layer_metadata_staging
+	select * from get_mol_layers('india_reptiles');
+	
+---------------------------------
+-- populate the ac_staging table
+---------------------------------
+
+--sanity check: both queries should have the same number of rows
+select distinct scientificname from india_reptiles
+
+select distinct m.scientificname as n,
+	t.common_names_eng as v
+from india_reptiles m left join taxonomy t 
+on m.scientificname = t.scientificname
+
+-- insert the rows
+insert into ac_staging
+	select distinct m.scientificname as n,
+		t.common_names_eng as v
+	from india_reptiles m left join taxonomy t 
+	on m.scientificname = t.scientificname
+
+---------------------------------
+-- dataset stats
+---------------------------------
+
+--num species: 196
+--num geometries: 1
+
+--species to test:
+--Bronchocela jubata
+
+
---
+++ staging/load_scripts/india_reptiles_surfacing_tables.sql
@@ -0,0 +1,149 @@
+﻿-- venugopal with provider e.g. 'stalmans'
+-- Venugopal with provider but capitalized, such as 'Stalmans'
+-- An updated and annotated list of Indian lizards (Reptilia: Sauria) based on a review of distribution records and checklists of Indian reptiles with title e.g. 'Tinleys plant species list for the greater Gorongosa ecosystem'
+-- india_reptiles with dataset id (table name) e.g 'gorongosa_flora'
+-- 2010 with year of publication e.g. '2010' 
+-- http://lacertilia.de/AS/Bibliografie/BIB_4464.pdf with the full url to the study e.g. http://oo.adu.org.za/content.php?id=4
+-- lacertilia.de with the short url to the study e.g. oo.adu.org.za
+-- reptiles with the lower-case tax. e.g. herps or aves
+-- Reptiles with the same as taxa column, but capitalize
+-- dilip@umd.edu with the email address of the author or organization
+-- India - the geographic extent eg. "Global" or "Angola" or "New World"
+--  with null if not applicable
+-- ca. 2008 with a string representation for the date range "ca. 1980 to 2010"
+-- Venugopal, P.D (2010). An updated and annoted list of Indian lizards (reptilia: Souria) based on a review of distribution records and checklists of Indian reptiles. Journal of Threatened Taxa.  2(3): 725-738
+-- none - null if none
+--  - null if none
+-- GADM - what was used for spatial information
+-- Reptiles - same as class but with common names "Plants", or "Birds"
+-- Reptiles: Murthy (1985), Murthy (1990a), Tikader & Sharma (1992), Das (1997), Das (2003), Das (1994), Das (1996a), Sharma (2002) with references for the taxonomy
+
+-- make sure there is not already a provider record for this provider
+-- if there is, tell Ben.
+select * from providers where provider = 'venugopal'
+
+insert into providers 
+	(provider,pubdate,title,url,source_type)
+values (
+	'venugopal', -- provider: all lower case. the id of the provider (if an article, use the author but not the date).  in the data_registry, this this is stored in the provider field.
+	null, -- pubdate: year published
+	null, -- title: String title I.e. "Birds of Melanesia". should mach data_registry.dataset_title and dashboard.dataset_title
+	null, --url: Link to the study
+
+	-- do not change below fields
+	'scilit' --source_type: <wwf | webuser | mol | gbif | iucn | ebird | scilit >
+)
+
+insert into data_registry
+	(dataset_id,table_name,geom_table,provider,taxa,classes,dataset_title,seasonality,auto_tax,type,geom_id,geom_link_id,
+		geometry_field,scientificname,ready,style_table,product_type,presence)
+values (
+	'india_reptiles', --dataset_id: the name of the species list table (same as table_name) e.g. "myanmar_flora"
+	'india_reptiles', --table_name: the name of the table that holds the species list. note same as dataset_id
+	'india_reptiles_geom', --geom_table: the name of the geom table.  should be <table_name>_geom eg "myanmar_flora_geom"	
+	'venugopal', --provider: the key to the provider table (column providers.provider) eg "hillers"	
+	'reptiles', --taxa: the taxonomic group, like "plants", "aves", herps, etc.  should match dashboard_metadata.class	
+	'Reptiles', -- classes: same as taxa column, but capitalize
+	'An updated and annotated list of Indian lizards (Reptilia: Sauria) based on a review of distribution records and checklists of Indian reptiles', --dataset_title - the publication name.  "Birds of Melanesia" should match providers.title and dashboard dataset.title
+	0, --seasonality: name of the seasonality field.  0 if none
+	true, --auto_tax: short for 'autonomous taxonomy'.  i.e. true if we use the tax in from the dataset, false if we use MOL tax.  usually true
+	
+	-- do not change fields below this line
+	'geochecklist', --type: geochecklist (means we have a spp table and a geometry table)
+	'geom_id', --geom_id: the foreign key in the species list. should be 'geom_id'
+	'cartodb_id', --geom_link_id: the primary key in the geom table.  should be 'cartodb_id'
+	'the_geom_webmercator', --geometry_field: should be: the_geom_webmercator
+	'scientificname', --scientificname: the column that holds the scientific name (scientifcname)
+	false, --ready: false until dataset is ready to surface
+	'polygons_style', -- style_table: always polygon_style for checklist data
+	'localinv', --product_type: always localinv for checklist data 
+	1 --presence - always set to 1 for checklist data
+)
+
+-- insert multiple rows into feature metadata.
+-- Always have the below fields:
+-- "Species name", mapped to scientificname field
+-- "Source", put the string literal in for data_registry.dataset_title
+-- "Provider", put the string literal in for <providers.provider>, <provider.pubdate> eg 'Hillers, 2009'
+-- "URL", CONCAT('<a target=''_melanesia'' onclick=''window.open(this.href)''href=''', final_url,'''>',final_url,'</a>')
+-- "Type", put'Local Inventory'
+-- Can include other fields if relevant.
+
+--delete from feature_metadata where data_table = 'india_reptiles' and title = 'Species name'
+
+--Species Name field
+insert into feature_metadata
+	(data_table,field,title,"order") --order and type are keywords so need to be in quotes
+values (
+	'india_reptiles', --data_table - the name of the species list table. should match data_registry.table_name
+
+	-- do not change fields below this line
+	'scientificname', --field - the name of the field to be displayed from the data_table
+	'Species name', -- title - the display name of the field
+	1 --order - how to order the fields from the data_table
+)
+
+-- delete from feature_metadata where data_table = 'india_reptiles' and title = 'Source'
+-- Source field
+insert into feature_metadata
+	(data_table,field,title,"order") --order and type are keywords so need to be in quotes
+values (
+	'india_reptiles', --data_table - the name of the species list table. should match data_registry.table_name
+	'''An updated and annotated list of Indian lizards (Reptilia: Sauria) based on a review of distribution records and checklists of Indian reptiles''', --field - the name of the field to be displayed from the data_table
+		
+	-- do not change fields below this line
+	'Source', -- title - the display name of the field
+	2 --order - how to order the fields from the data_table
+)
+-- delete from feature_metadata where data_table = 'india_reptiles' and title = 'Provider'
+-- Provider field
+insert into feature_metadata
+	(data_table,field,title,"order") --order and type are keywords so need to be in quotes
+values (
+	'india_reptiles', --data_table - the name of the species list table. should match data_registry.table_name
+	'''Venugopal, 2010''', --field - the name of the field to be displayed from the data_table
+		
+	-- do not change fields below this line
+	'Provider', -- title - the display name of the field
+	3 --order - how to order the fields from the data_table
+)
+
+-- delete from feature_metadata where data_table = 'india_reptiles' and title = 'URL'
+-- URL field
+insert into feature_metadata
+	(data_table,field,title,"order") --order and type are keywords so need to be in quotes
+values (
+	'india_reptiles', --data_table - the name of the species list table. should match data_registry.table_name
+	'''<a target=''''_blank'''' onclick=''''window.open(this.href)'''' href=''''http://lacertilia.de/AS/Bibliografie/BIB_4464.pdf''''>lacertilia.de</a>''', --field - the name of the field to be displayed from the data_table
+		
+	-- do not change fields below this line
+	'URL', -- title - the display name of the field
+	4 --order - how to order the fields from the data_table
+)
+
+-- dashboard_metadata
+--delete from dashboard_metadata where dataset_id = 'india_reptiles'
+insert into dashboard_metadata
+	("class",provider,contact,coverage,dataset_id,date_more,date_range,description,recommended_citation,
+		seasonality,seasonality_more,spatial_metadata,taxon,taxonomy_metadata,url,"type")
+values (
+	'reptiles', --class - the taxonomic group, like "plants" or "aves".  should match data_registry.taxa
+	'venugopal', --provider
+	'dilip@umd.edu', --contact - email address of contact
+	'India', --coverage - the geographic extent eg. "Global" or "Angola" or "New World"
+	'india_reptiles', --dataset_id - the name of the species list table
+	null, --  with null if not applicable
+	'ca. 2008', --ca. 2008 with a string representation for the date range "ca. 1980 to 2010"
+	'An updated and annotated list of Indian lizards (Reptilia: Sauria) based on a review of distribution records and checklists of Indian reptiles', --description - string description
+	'Venugopal, P.D (2010). An updated and annoted list of Indian lizards (reptilia: Souria) based on a review of distribution records and checklists of Indian reptiles. Journal of Threatened Taxa.  2(3): 725-738', --recommended_citation 
+	'none', -- none - null if none
+	null, --  - null if none
+	'GADM', -- GADM - what was used for spatial information
+	'Reptiles', -- Reptiles - same as class but with common names "Plants", or "Birds"
+	'Reptiles: Murthy (1985), Murthy (1990a), Tikader & Sharma (1992), Das (1997), Das (2003), Das (1994), Das (1996a), Sharma (2002)', --taxonomy_metadata: references for the taxonomy
+	'http://lacertilia.de/AS/Bibliografie/BIB_4464.pdf', --url - url of the datasource
+
+	-- do not change fields below this line
+	'localinv' --type - localinv. should match data_registry.product_type	
+)
+
