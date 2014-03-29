5fb0891c4184c3fdacdfb1eaf9c898225f2cfc9e
Ijeamakaanyene <Ijeamaka.Anyene@gmail.com>
2014-03-28 23:45:16 -0400

    updated and are working on csv and sql

--- staging/load_scripts/kwamgumi_mammals.sql
+++ staging/load_scripts/kwamgumi_mammals.sql
@@ -1,7 +1,7 @@
 ﻿--F5 - Execute selection
 --Replace All 
--- fourni_reptiles with the species table name
--- dimaki with provider e.g. 'stalmans'
+-- kwamgumi_mammals with the species table name
+-- stanley with provider e.g. 'stalmans'
 
 ---------------------------
 -- create the species table
@@ -13,87 +13,75 @@
 --example: golarainforest_herps
 
 --create the column that will link from the geom table (primary key) to species list (foreign key)
-alter table fourni_reptiles
+alter table kwamgumi_mammals
 add column geom_id integer;
 
 -- create an index on the geom id <tablename>_<colname>_btree
-drop index if exists fourni_reptiles_geom_id_btree;
-create index fourni_reptiles_geom_id_btree ON fourni_reptiles (geom_id);
+drop index if exists kwamgumi_mammals_geom_id_btree;
+create index kwamgumi_mammals_geom_id_btree ON kwamgumi_mammals (geom_id);
 
 --create an index on the scientific name  <tablename>_scientificname_btree
-drop index if exists fourni_reptiles_scientificname_btree;
-create index fourni_reptiles_scientificname_btree ON fourni_reptiles (scientificname);
+drop index if exists kwamgumi_mammals_scientificname_btree;
+create index kwamgumi_mammals_scientificname_btree ON kwamgumi_mammals (scientificname);
 
 -------------------------------------------------------------------------
 -- fix data if required - better to do in excel and re-upload if possible
 -------------------------------------------------------------------------
 
 
-----------------------
---check data
-----------------------
-
-SELECT * from fourni_reptiles where scientificname ilike '%/%'
-
----check to see if any distribution rows don't match
-SELECT m.dist
-FROM fourni_reptiles m
-LEFT JOIN fourni_reptiles_geom_1 g
-on g.geom_name = m.dist
-where g.geom_name IS NULL
-group by m.dist
-
-SELECT m.dist
-FROM fourni_reptiles m
-LEFT JOIN fourni_archipelago_islands g
-on g.geom_name = m.dist
-where g.geom_name IS NULL
-group by m.dist
-
 ------------------------
 -- create the geom table
 ------------------------
-alter table fourni_reptiles_geom
-add column geom_name varchar(40);
+--1. usig gadm
+--2. using wdpa
+--3. using a shapefile
 
-insert into fourni_reptiles_geom (geom_name, the_geom)
-select m.dist as geom_name, st_multi(st_union(g.the_geom)) as the_geom
-from fourni_archipelago_islands g
-join (select distinct dist from fourni_reptiles) m
-on g.geom_name = m.dist
-group by m.dist;
+--2. using wdpa
 
-alter table fourni_reptiles_geom_1
-add column geom_name varchar(40);
- 
-insert into fourni_reptiles_geom_1
-select * from fourni_reptiles_geom_2
+--use cartodb webui to create the geom table. <tablename>_geom  or use below "create table from query" option in cartodb.
+--this query will work if the is a single polygon for the checklist.
 
-insert into fourni_reptiles_geom (geom_name, the_geom)
-select m.dist as geom_name, st_multi(st_union(g.the_geom)) as the_geom
-from fourni_reptiles_geom_1 g
-join (select distinct dist from fourni_reptiles) m 
-on g.geom_name = m.dist
-group by m.dist; 
+select new_new_name as geom_name, the_geom
+from wdpa2010
+where new_new_name = 'Kwamgumi'
+
+
+--3. used a supplied shapfile
+
+-- create the geom table use "create table from query" button in cartodb
+-- merge multiple records into a single record
+select geom_name as geom_name, st_multi(st_union(the_geom)) as the_geom
+from kwamgumi_mammals_shapefile
+group by geom_name;
 
 -------------------------------------------
 -- link the species table to the geom table
 -------------------------------------------
 
---add in all of the id's from the geometry table.
-update fourni_reptiles c
-set geom_id = g.cartodb_id
-from fourni_reptiles_geom g
-where c.dist = g.geom_name
+-- first check to see if any distribution rows don't match
+-- this query should come back empty
+-- not needed if there is only one geometry to link to
+SELECT  m.dist
+FROM    kwamgumi_mammals m 
+LEFT JOIN kwamgumi_mammals_geom g
+ON      g.geom_name = m.dist
+WHERE   g.geom_name IS NULL
+group by m.dist
 
---quick check to make sure it worked
-select c.geom_id, g.cartodb_id, c.dist, g.geom_name
-from fourni_reptiles c
-left join fourni_reptiles_geom g
-on c.dist = g.geom_name
+-- could check the other way - are the rows in the geometry table that aren't in species table?
+
+--add in all of the id's from the geometry table.
+update kwamgumi_mammals m
+set geom_id = g.cartodb_id
+from kwamgumi_mammals_geom g
+where m.dist = g.geom_name
+
+-- if there is only one geometry, set geom_id to the appropriate cartodb id
+update kwamgumi_mammals m
+set geom_id = 1
 
 --set to not null.  extra check to make sure all rows matched.
-alter table fourni_reptiles
+alter table kwamgumi_mammals
 alter column geom_id set not null;
 
 -------------------
@@ -104,7 +92,7 @@ alter column geom_id set not null;
 --this means that all geometries are valid and don't have internal issues
 --if there is another line that has 'f' anywhere then tell Ben.  This means the table has invalid geometries
 select count(*) as num, st_isvalid(the_geom) as valid_geom, st_isvalid(the_geom_webmercator) as valid_geom_wm 
-from fourni_reptiles_geom group by valid_geom, valid_geom_wm
+from kwamgumi_mammals_geom group by valid_geom, valid_geom_wm
 
 -- make species list and geom table public in cartodb
 
@@ -119,18 +107,18 @@ from fourni_reptiles_geom group by valid_geom, valid_geom_wm
 -----------------------
 
 -- test in cartodb, get_tile should map te specis
-SELECT * FROM get_tile('dimaki', 'localinv', 'Ophisops elegans','fourni_reptiles')
+SELECT * FROM get_tile('stanley', 'localinv', 'Crocidura elgonius','kwamgumi_mammals')
 
 -- sql to if get_tile does not work.
-SELECT * from data_registry WHERE provider = 'dimaki' and type = 'localinv' or table_name = 'fourni_reptiles'
+SELECT * from data_registry WHERE provider = 'stanley' and type = 'localinv' or table_name = 'kwamgumi_mammals'
 
 SELECT d.*,g.*
-  FROM fourni_reptiles d
-  JOIN fourni_reptiles_geom g ON 
+  FROM kwamgumi_mammals d
+  JOIN kwamgumi_mammals_geom g ON 
   d.geom_id = g.cartodb_id
   where d.scientificname = 'Azolla nilotica';
 
-select * from fourni_reptiles where scientificname = 'Azolla nilotica';	
+select * from kwamgumi_mammals where scientificname = 'Azolla nilotica';	
 	  
 			  
 -------------------------------------------
@@ -138,35 +126,35 @@ select * from fourni_reptiles where scientificname = 'Azolla nilotica';
 -------------------------------------------
 
 insert into layer_metadata_staging
-	select * from get_mol_layers('fourni_reptiles');
+	select * from get_mol_layers('kwamgumi_mammals');
 	
 ---------------------------------
 -- populate the ac_staging table
 ---------------------------------
 
 --sanity check: both queries should have the same number of rows
-select distinct scientificname from fourni_reptiles
+select distinct scientificname from kwamgumi_mammals
 
 select distinct m.scientificname as n,
 	t.common_names_eng as v
-from fourni_reptiles m left join taxonomy t 
+from kwamgumi_mammals m left join taxonomy t 
 on m.scientificname = t.scientificname
 
 -- insert the rows
 insert into ac_staging
 	select distinct m.scientificname as n,
 		t.common_names_eng as v
-	from fourni_reptiles m left join taxonomy t 
+	from kwamgumi_mammals m left join taxonomy t 
 	on m.scientificname = t.scientificname
 
 ---------------------------------
 -- dataset stats
 ---------------------------------
 
---num species: 7
---num geometries: 7
+--num species: 13
+--num geometries: 1
 
 --species to test:
---Ophisops elegans
+--Crocidura elgonius
 
 
---
+++ staging/load_scripts/ngaramia_all.sql
@@ -0,0 +1,160 @@
+﻿--F5 - Execute selection
+--Replace All 
+-- ngaramia_all with the species table name
+-- doggart with provider e.g. 'stalmans'
+
+---------------------------
+-- create the species table
+---------------------------
+
+--upload the xlsx into cartodb
+
+--rename if necessary w/in cartodb ui.  format should be <place>_<taxa>
+--example: golarainforest_herps
+
+--create the column that will link from the geom table (primary key) to species list (foreign key)
+alter table ngaramia_all
+add column geom_id integer;
+
+-- create an index on the geom id <tablename>_<colname>_btree
+drop index if exists ngaramia_all_geom_id_btree;
+create index ngaramia_all_geom_id_btree ON ngaramia_all (geom_id);
+
+--create an index on the scientific name  <tablename>_scientificname_btree
+drop index if exists ngaramia_all_scientificname_btree;
+create index ngaramia_all_scientificname_btree ON ngaramia_all (scientificname);
+
+-------------------------------------------------------------------
+-- fix data if required - better to do in excel and re-upload if possible
+-------------------------------------------------------------------
+
+
+------------------------
+-- create the geom table
+------------------------
+--1. usig gadm
+--2. using wdpa
+--3. using a shapefile
+
+--2. using wdpa
+
+--use cartodb webui to create the geom table. <tablename>_geom  or use below "create table from query" option in cartodb.
+--this query will work if the is a single polygon for the checklist.
+
+select geom_id as geom_name, the_geom
+from ngaramia_all_shapefile
+where geom_id = 'Somangira'
+
+
+--3. used a supplied shapfile
+
+-- create the geom table use "create table from query" button in cartodb
+-- merge multiple records into a single record
+select geom_id as geom_name, st_multi(st_union(the_geom)) as the_geom
+from ngaramia_all_shapefile
+group by geom_name;
+
+-------------------------------------------
+-- link the species table to the geom table
+-------------------------------------------
+
+-- first check to see if any distribution rows don't match
+-- this query should come back empty
+-- not needed if there is only one geometry to link to
+SELECT  m.dist
+FROM    ngaramia_all m 
+LEFT JOIN ngaramia_all_geom g
+ON      g.geom_name = m.dist
+WHERE   g.geom_name IS NULL
+group by m.dist
+
+-- could check the other way - are the rows in the geometry table that aren't in species table?
+
+--add in all of the id's from the geometry table.
+update ngaramia_all m
+set geom_id = g.cartodb_id
+from ngaramia_all_geom g
+where m.dist = g.geom_name
+
+-- if there is only one geometry, set geom_id to the appropriate cartodb id
+update ngaramia_all m
+set geom_id = 1
+
+--set to not null.  extra check to make sure all rows matched.
+alter table ngaramia_all
+alter column geom_id set not null;
+
+-------------------
+-- final checks ---
+-------------------
+
+--this line should return only one line that has 't' for valid_geom and valid_geom_wm
+--this means that all geometries are valid and don't have internal issues
+--if there is another line that has 'f' anywhere then tell Ben.  This means the table has invalid geometries
+select count(*) as num, st_isvalid(the_geom) as valid_geom, st_isvalid(the_geom_webmercator) as valid_geom_wm 
+from ngaramia_all_geom group by valid_geom, valid_geom_wm
+
+-- make species list and geom table public in cartodb
+
+-----------------------
+-- populate all the surfacing tables
+-----------------------
+
+-- use other sql template
+
+-----------------------
+-- Test out get_tile, should show up in cartodb visualization
+-----------------------
+--stopped somewhere around here, have no idea what went wrong
+-- test in cartodb, get_tile should map te specis
+SELECT * FROM get_tile('doggart', 'localinv', 'Panthera pardus','ngaramia_all')
+
+-- sql to if get_tile does not work.
+SELECT * from data_registry WHERE provider = 'doggart' and type = 'localinv' or table_name = 'ngaramia_all'
+
+SELECT d.*,g.*
+  FROM ngaramia_all d
+  JOIN ngaramia_all_geom g ON 
+  d.geom_id = g.cartodb_id
+  where d.scientificname = 'Papio cynocephalus';
+
+select * from ngaramia_all where scientificname = 'Papio cynocephalus';	
+	  
+			  
+-------------------------------------------
+-- populate the layer_metadata_staging table
+-------------------------------------------
+
+insert into layer_metadata_staging
+	select * from get_mol_layers('ngaramia_all');
+	
+---------------------------------
+-- populate the ac_staging table
+---------------------------------
+
+--sanity check: both queries should have the same number of rows
+select distinct scientificname from ngaramia_all
+
+select distinct m.scientificname as n,
+	t.common_names_eng as v
+from ngaramia_all m left join taxonomy t 
+on m.scientificname = t.scientificname
+
+-- insert the rows
+insert into ac_staging
+	select distinct m.scientificname as n,
+		t.common_names_eng as v
+	from ngaramia_all m left join taxonomy t 
+	on m.scientificname = t.scientificname
+
+---------------------------------
+-- dataset stats
+---------------------------------
+
+--num species: 306
+--num geometries: 1
+
+--species to test:
+--Peliperdix coqui
+
+
